ddcf37d386e90716924cc422130a36e611b682e6
阿草正气水 <coderco@foxmail.com>
2022-03-28 14:32:17 +0800

    add 编译四则运算 parser generator tokens

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-export const one = 1
-export const two = 2
+export function analysis(expression: string): any[] {
+  const symbol = ['(', ')', '+', '-', '*', '/']
+  const re = /\d/
+
+  const tokens: string[] = []
+  const chars = expression.trim().split('')
+
+  let token = ''
+
+  chars.forEach((char) => {
+    //  是数字 遍历到下一个字符 看下一个字符是否是数字
+    if (re.test(char)) {
+      token += char
+    }
+    else if (char === ' ' && token) {
+      //  每个 if 都中断上一个 if 的执行
+      tokens.push(token)
+      token = ''
+    }
+    else if (symbol.includes(char)) {
+      if (token) {
+        tokens.push(token)
+        token = ''
+      }
+      // 添加符号
+      tokens.push(char)
+    }
+  })
+
+  if (token) tokens.push(token)
+
+  return tokens
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,28 @@
 import { describe, expect, it } from 'vitest'
+import { analysis } from '../src'
+import { AssemblyWriter, Parser } from '../src/AssemblyWriter'
 
-describe('should', () => {
+describe('词法分析', () => {
   it('exported', () => {
-    expect(1).toEqual(1)
+    // const tokens = analysis('100+10*10')
+    const tokens = analysis('100    +   23   +    34 * 10 / 2')
+
+    const writer = new AssemblyWriter()
+    const parser = new Parser(tokens, writer)
+    const instructions = parser.getInstructions()
+
+    expect(tokens).toEqual([
+      '100',
+      '+',
+      '23',
+      '+',
+      '34',
+      '*',
+      '10',
+      '/',
+      '2',
+    ])
+
+    expect(instructions).toEqual('push 100\r\npush 23\r\nadd\r\npush 34\r\npush 10\r\nmul\r\npush 2\r\ndiv\r\nadd\r\n')
   })
 })
